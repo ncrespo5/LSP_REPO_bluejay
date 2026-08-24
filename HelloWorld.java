c17a59de2d871eb433d318881ecebe2dbeaa0a7a
Nyla Crespo <143649526+ncrespo5@users.noreply.github.com>
2026-08-24 17:20:38 -0400

    upload hello world

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * A program that prints Hello World.
+ *
+ * @author Nyla Crespo
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
